81c055c72688a02790e4d59ded434cf124e69cd8
JazzyMussels <jeffmussels@gmail.com>
2019-08-28 19:50:49 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Zoo
+  attr_reader :name, :location
+  @@all = []
+
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.find_by_location(located)
+    self.all.select{|animal| animal.location == located}
+  end
+
+  def animals
+    Animal.all.select{|animal| animal.zoo_name == self.name}
+  end
+
+  def animal_species
+    animals.map{|animal| animal.species}.uniq
+  end
+
+  def find_by_species(species_name)
+    animals.map{|animal| animal if animal.species == species_name}
+  end
+
+  def animal_nicknames
+    animals.map{|animal| animal.nickname}
+  end
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,15 @@ require 'pry'
 
 #Test your code here
 
+puts "done"
+
+zoo1 = Zoo.new('Bronx Zoo', 'NYC')
+zoo2 = Zoo.new('Brooklyn Zoo', 'NYC')
+zoo3 = Zoo.new('Norfolk Zoo', 'Norfolk')
+bear = Animal.new('Bear', 350, 'big boy', 'Brooklyn Zoo')
+penguin = Animal.new('Penguin', 40, 'beaky', 'Bronx Zoo')
+lion = Animal.new('Lion', 200, 'king', 'Norfolk Zoo')
+
+
 
 binding.pry
-puts "done"
