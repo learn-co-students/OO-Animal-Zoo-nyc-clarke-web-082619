18f4a0a02c797a1f25c08a696c9e57e7b63b3a6a
castagnaTM <castagna.tm@gmail.com>
2019-09-03 11:06:16 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,47 @@
 class Zoo
 
+    attr_accessor :name, :location, :animal
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def add_animal(species, weight, nickname)
+        animal = Animal.new(species, weight, nickname)
+        animal.zoo = self
+    end
+
+    def add_animal_by_name(nickname)
+        Animal.all.select { |animal| animal.zoo = self if animal.nickname == nickname }
+    end
+
+    def animals
+        Animal.all.select {|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        animals.map { |animal| animal.species }.uniq
+    end
+
+    def find_by_species(species)
+        animals.select { |animal| animal if animal.species == species }
+    end
+
+    def animal_nicknames
+        animals.map { |animal| animal.nickname }
+    end
+
+    def self.find_by_location(location)
+        all.select { |zoo| zoo.location == location }
+    end
+
+    def self.all
+        @@all
+    end
+
+
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,21 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+an1 = Animal.new("Tiger", 120, "Swipey")
+an2 = Animal.new("Lion", 130, "Loud-Boy")
+an3 = Animal.new("Elephant", 400, "Trunks")
+an4 = Animal.new("Otter", 20, "Squirmy")
+an5 = Animal.new("Human", 155, "Lawrence")
 
+zoo1 = Zoo.new("Big Cats", "New York")
+zoo2 = Zoo.new("Cuties", "New York")
+zoo3 = Zoo.new("Fake Animal Rescue", "Nevada")
+
+zoo1.add_animal_by_name("Swipey")
+zoo1.add_animal_by_name("Loud-Boy")
+zoo2.add_animal_by_name("Trunks")
+zoo2.add_animal_by_name("Squirmy")
+zoo3.add_animal_by_name("Lawrence")
 
 binding.pry
 puts "done"
