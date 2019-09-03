afa3e08077bc577962fea97ff5e84be0b58466df
github username <github email address>
2019-09-03 13:53:24 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,20 @@
 class Animal
+    attr_accessor :nickname, :weight, :species,:zoo
+    @@all = []
 
+    def initialize(nickname, weight, species,zoo)
+        @nickname = nickname
+        @weight = weight
+        @species = species
+        @zoo = zoo
+        @@all << self
+    end
+
+    def self.find_by_species(species)
+        @@all.select{|animal| animal.species == species}
+    end
+
+    def self.all
+        @@all
+    end
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,37 @@ require 'pry'
 
 #Test your code here
 
+zoo1 = Zoo.new("bobbie1", "USA")
+zoo2 = Zoo.new("bobbie2", "Japan")
+zoo3 = Zoo.new("bobbie3", "Canada")
+zoo4 = Zoo.new("bobbie4", "Africa")
+zoo5 = Zoo.new("bobbie5", "China")
+zoo6 = Zoo.new("bobbie6", "Korea")
+zoo7 = Zoo.new("bobbie7", "Europe")
+
+
+animal1 = Animal.new("bob1",100, "Dog", zoo1)
+animal2 = Animal.new("bob2",101, "Dog", zoo1)
+animal3 = Animal.new("bob3",120, "Cat", zoo1)
+animal4 = Animal.new("bob4",103, "Cat", zoo1)
+animal5 = Animal.new("bob5",140, "Lion", zoo2)
+animal6 = Animal.new("bob6",105, "Dog", zoo2)
+animal7 = Animal.new("bob7",160, "Cat", zoo2)
+animal8 = Animal.new("bob8",107, "Lion", zoo2)
+animal9 = Animal.new("bob9",108, "Cat", zoo3)
+animal10 = Animal.new("bob10",109, "Dog", zoo3)
+animal11 = Animal.new("bob11",110, "Rat", zoo3)
+animal12 = Animal.new("bob12",101, "Dog", zoo4)
+animal13 = Animal.new("bob13",4213, "Lion", zoo4)
+animal14 = Animal.new("bob14",53125, "Cat", zoo5)
+animal15 = Animal.new("bob15",5312, "Cat", zoo6)
+animal16 = Animal.new("bob16",64, "Dog", zoo7)
+animal17 = Animal.new("bob17",13214, "Lion", zoo7)
+animal18 = Animal.new("bob18",143, "Cat", zoo7)
+
+
+
+
 
 binding.pry
 puts "done"
